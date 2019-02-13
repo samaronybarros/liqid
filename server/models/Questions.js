c4c8ba4ad838eab31da9a801354e61d5c7178868
Sam Barros <samarony.barros@gmail.com>
2019-02-13 12:49:24 +0100

    Removes unecessary comments

--- server/models/Questions.js
+++ server/models/Questions.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-// create new instance of the mongoose.schema. the schema takes an
-// object that shows the shape of your database entries.
 const QuestionsSchema = new Schema(
     {
         question: String,
@@ -11,5 +9,4 @@ const QuestionsSchema = new Schema(
     { timestamps: true }
 )
 
-// export our module to use in server.js
 module.exports = mongoose.model('Questions', QuestionsSchema)
