5643cfd23317669ecfefe960e6415b5d6d21f434
Sam Barros <samarony.barros@gmail.com>
2019-02-14 17:35:06 +0100

    creates default Style

---
+++ client/src/components/elements/Style/index.js
@@ -0,0 +1,8 @@
+export default {
+    primaryColor: '#000092',
+    secondaryColor: '#FFFFFF',
+    panelBackgroundColor: '#FFFFFF',
+    bodyBackGroundColor: '#EEEEEE',
+
+    shadow: '#C9C9C9',
+}
